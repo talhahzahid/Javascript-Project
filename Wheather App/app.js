14a8e4cf7ec9db3ca0eee3810eb5f8f4b5ddef10
talhahzahid <talhazahid415@gmail.com>
2024-10-08 14:47:20 +0500

    push origin main

--- Wheather App/app.js
+++ Wheather App/app.js
@@ -5,10 +5,13 @@ const form = document.querySelector("form");
 const input = document.querySelector("#input");
 const div = document.querySelector('div');
 
+
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     console.log(input.value);
     function checkWheather() {
+        div.innerHTML = ""
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=b6b7b0805e7a8277f737932bc79cca5f `)
             .then((res) => {
                 return res.json();
@@ -16,15 +19,16 @@ form.addEventListener("submit", (event) => {
                 console.log(data.main);
                 console.log(data.name);
                 div.innerHTML += `
-            <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h class="card-text">${data.name}</h>
-    <h class="card-text">${data.main.temp}</h>
-    <h class="card-text">${data.main.
-        pressure}</h>
+            <div class="card bg-transparent border border-info" style="width: 30rem;">
+  <div class="card-body text-center text-white ">
+    <h1 class="card-text">${data.name}</h1>
+    <h1 class="card-text">Temperature : ${data.main.temp}</h1>
+    <h1 class="card-text">Pressure : ${data.main.pressure}</h1>
+    <h1 class="card-text">humidity : ${data.main.humidity}</h1>
+    <h1 class="card-text">Temperature Max : ${data.main.temp_max}</h1>
+    <h1 class="card-text">Temperature Min : ${data.main.temp_min}</h1>
   </div>
 </div>
-
              `
             })
             .catch((err) => {
@@ -34,4 +38,5 @@ form.addEventListener("submit", (event) => {
     checkWheather()
     input.value = ''
 })
+
 
